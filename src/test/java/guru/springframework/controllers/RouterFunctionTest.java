6109dd485abc55fd6e89c47045cce7e92b9a9740
Bohdan Kalika <bogdan.kalika@gmail.com>
2024-05-07 16:59:16 +0300

    Finished Section 22.

--- src/test/java/guru/springframework/controllers/RouterFunctionTest.java
+++ src/test/java/guru/springframework/controllers/RouterFunctionTest.java
@@ -5,14 +5,16 @@ import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.server.RouterFunction;
 import reactor.core.publisher.Flux;
 
 import static reactor.core.publisher.Mono.when;
@@ -21,28 +23,37 @@ import static reactor.core.publisher.Mono.when;
  * Created by bogdan.kalika@gmail.com
  * Date: 5/3/2024
  */
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureWebTestClient
 public class RouterFunctionTest {
 
+    ApplicationContext applicationContext;
+
     WebTestClient webTestClient;
 
+    @Mock
     RecipeService recipeService;
 
-    @Autowired
     WebConfig webConfig;
+
+    @Autowired
+    public RouterFunctionTest(ApplicationContext applicationContext, WebConfig webConfig) {
+        this.applicationContext = applicationContext;
+        this.webConfig = webConfig;
+    }
 
     @BeforeEach
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
-//        webTestClient = WebTestClient
-//                .bindToRouterFunction(webConfig.router(recipeService))
-//                .build();
+        webTestClient = WebTestClient
+                .bindToApplicationContext(applicationContext)
+                .build();
     }
 
     @Test
-    public void testGetRecipes() {
+    public void testGetRecipes() throws Exception {
         when(recipeService.getRecipes()).thenReturn(Flux.just());
 
         webTestClient.get().uri("/api/recipes")
